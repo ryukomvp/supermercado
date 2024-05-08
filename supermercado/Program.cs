61fa6aeed85976b2c08474795398effdd1ee6c2f
ryukomvp <daniel123hernandez15@gmail.com>
2024-05-08 00:20:58 -0600

    Archivos

---
+++ supermercado/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: danie
+ * Date: 08/05/2024
+ * Time: 0:19
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace supermercado
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
