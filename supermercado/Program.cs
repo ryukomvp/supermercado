9047abb2b39c96cef6dac173e016568d9ce35ecd
ryukomvp <daniel123hernandez15@gmail.com>
2024-05-08 01:18:04 -0600

    Avances

--- supermercado/Program.cs
+++ supermercado/Program.cs
@@ -7,19 +7,158 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.IO;
 
 namespace supermercado
 {
 	class Program
 	{
+		public struct producto
+		{
+			public string nombre;
+			public Double precio;
+			public string cat;
+			
+			public string[] categorias;
+		}
+		
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.Title = "Supermercado";
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.BackgroundColor = ConsoleColor.Black;
+			Console.Clear();
 			
-			// TODO: Implement Functionality Here
+			producto var_producto;
+//			var_producto.categorias = {"Abarrotes", "Bebidas", "Carnes", "Embutidos", "Frutas", "Verduras"};
+			var_producto.categorias = new string[6];
+			var_producto.categorias[0] = "Abarrotes";
+			var_producto.categorias[1] = "Bebidas";
+			var_producto.categorias[2] = "Carnes";
+			var_producto.categorias[3] = "Embutidos";
+			var_producto.categorias[4] = "Frutas";
+			var_producto.categorias[5] = "Verduras";
 			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
+			producto[] array_producto = new producto[5];
+			for (int i = 0; i < 5; i++) {
+				Console.WriteLine("\n\tProducto N°{0}", i+1);
+				Console.Write("\n\tIngrese el nombre del producto: ");
+				array_producto[i].nombre = Console.ReadLine();
+				Console.Write("\tIngrese el precio del producto (##,##) : $");
+				array_producto[i].precio = Double.Parse(Console.ReadLine());
+				for (int j = 0; j < 6; j++) {
+					Console.Write("\n\t{0}. {1}", j+1, var_producto.categorias[j]);
+				}
+				Console.Write("\n\n\tSeleccione la categoria: ");
+				int opcion_cat = int.Parse(Console.ReadLine());
+				array_producto[i].cat = var_producto.categorias[opcion_cat - 1];
+				Console.Clear();
+			}
+			
+			do
+			{
+				Console.Clear();
+				Console.Write("\n\t¿Qué producto quiere mostrar? ");
+				int n = int.Parse(Console.ReadLine());
+				Console.Clear();
+				imprimir(n, array_producto[n-1].nombre, array_producto[n-1].precio, array_producto[n-1].cat);
+			} while(salir());
+			
+			Console.Clear();
+			Console.Write("¿Quiere guardar los registros en un archivo? (Y/N)");
+			char resp = Console.ReadLine();
+			if (resp == 'Y' || resp == 'y' ) {
+				
+			}
+			creditos();
+		}
+		
+		static void imprimir(int n, string a, Double b, string c)
+		{
+			try {
+				// N° de prod
+				Console.Write("\n\tProducto ");
+				Console.ForegroundColor = ConsoleColor.Black;
+				Console.BackgroundColor = ConsoleColor.DarkGray;
+				Console.Write("N°{0}", n);
+				Console.ForegroundColor = ConsoleColor.White;
+				Console.BackgroundColor = ConsoleColor.Black;
+				// Nombre
+				Console.Write("\n\tNombre del producto: ");
+				Console.ForegroundColor = ConsoleColor.Black;
+				Console.BackgroundColor = ConsoleColor.Red;
+				Console.Write(a);
+				Console.ForegroundColor = ConsoleColor.White;
+				Console.BackgroundColor = ConsoleColor.Black;
+				// Precio
+				Console.Write("\n\tPrecio del producto: ");
+				Console.ForegroundColor = ConsoleColor.Black;
+				Console.BackgroundColor = ConsoleColor.Green;
+				Console.Write("${0}", b);
+				Console.ForegroundColor = ConsoleColor.White;
+				Console.BackgroundColor = ConsoleColor.Black;
+				// Categoria
+				Console.Write("\n\tCategoría del producto: ");
+				Console.ForegroundColor = ConsoleColor.Black;
+				Console.BackgroundColor = ConsoleColor.Blue;
+				Console.Write(c);
+				Console.ForegroundColor = ConsoleColor.White;
+				Console.BackgroundColor = ConsoleColor.Black;
+			} catch (Exception e) {
+				Console.ForegroundColor = ConsoleColor.White;
+				Console.BackgroundColor = ConsoleColor.Red;
+				Console.Write("\t");
+				Console.WriteLine(e.Message);
+				Console.ForegroundColor = ConsoleColor.White;
+				Console.BackgroundColor = ConsoleColor.Black;
+			}
+		}
+		
+		static void archivo()
+		{
+		
+		}
+		
+		static Boolean salir()
+		{
+			try {
+				Console.Write("\n\t¿Desea mostrar otro producto? [Y/N]: ");
+				char resp = Console.ReadKey().KeyChar;
+				
+				if (resp == 'Y' || resp == 'y') {
+					return true;
+				} else {
+					creditos();
+					return false;
+				}
+			} catch (Exception e) {
+				Console.ForegroundColor = ConsoleColor.White;
+				Console.BackgroundColor = ConsoleColor.Red;
+				Console.Write("\t");
+				Console.WriteLine(e.Message);
+				Console.ForegroundColor = ConsoleColor.White;
+				Console.BackgroundColor = ConsoleColor.Black;
+				
+				return false;
+			}
+		}
+		
+		static void creditos()
+		{
+			Console.Clear();
+			Console.WriteLine("\n\tGracias por su preferencia.");
+			Console.ForegroundColor = ConsoleColor.DarkGray;
+			Console.WriteLine("\tig:\thttps://www.instagram.com/dnlhernandez_/");
+			Console.WriteLine("\tgithub:\thttps://github.com/ryukomvp");
+			Console.ForegroundColor = ConsoleColor.Black;
+			Console.BackgroundColor = ConsoleColor.White;
+			Console.WriteLine("\n-> Fin del programa.");
+			Console.ReadKey();
 		}
 	}
 }
